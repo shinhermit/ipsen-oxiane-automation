6225e8177d6ba61e15e229304f27b2ac27b6057a
Sebastien Poirot <sebastien.poirot@gmx.fr>
2017-11-14 14:12:31 +0100

    implete google search console synchronisation with Google Analytics using the API

--- src/settings.py
+++ src/settings.py
@@ -1,9 +1,17 @@
 googleapi = {
+    "credentials": {
+        "client_secret_path": "etc/credentials/client_secret.json"
+    },
     "analytics": {
         "api_name": "analytics",
         "api_version": "v3",
         "scopes": ["https://www.googleapis.com/auth/analytics.edit",
-                   "https://www.googleapis.com/auth/analytics.readonly"],
-        "client_secret_path": "etc/credentials/client_secret.json"
+                   "https://www.googleapis.com/auth/analytics.readonly"]
+    },
+    "search_console": {
+        "api_name": "webmasters",
+        "api_version": "v3",
+        "scopes": ['https://www.googleapis.com/auth/webmasters',
+                   'https://www.googleapis.com/auth/webmasters.readonly']
     }
 }
--- src/sync_search_console_on_analytics.py
+++ src/sync_search_console_on_analytics.py
@@ -5,7 +5,29 @@ properties declared in Google Analytics
 
 from src import settings
 from src.googleapi.api_connector import get_service
+from src.googleapi.analyticss.data_models import AccountSummaryList
 
 
-analytics_settings = settings.googleapi["analytics"]
-api = get_service(analytics_settings["api_name"])
+def main():
+    client_secrets_path = settings.googleapi["credentials"]['client_secrets_path']
+
+    analytics_settings = settings.googleapi["analytics"]
+    api_analytics = get_service(api_name=analytics_settings["api_name"],
+                                api_version=analytics_settings['api_version'],
+                                client_secrets_path=client_secrets_path,
+                                scope=analytics_settings['scopes'])
+    search_console_settings = settings.googleapi["search_console"]
+
+    api_search_console = get_service(api_name=search_console_settings["api_name"],
+                                     api_version=search_console_settings['api_version'],
+                                     client_secrets_path=client_secrets_path,
+                                     scope=search_console_settings['scopes'])
+
+    account_summaries = api_analytics.management().accountSummaries().list().execute(num_retries=10)
+    acc_list = AccountSummaryList(account_summaries)
+
+    for account in acc_list.items:
+        for web_property in account.web_properties:
+            api_search_console.sites().add(siteUrl=web_property.website_url).execute()
+
+
