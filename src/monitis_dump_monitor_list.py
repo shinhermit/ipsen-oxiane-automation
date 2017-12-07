e0a39a878e0c7f43b0a3a02049e8b67fd114b5db
Josuah ARON <josuah.aron@gmail.com>
2017-12-07 09:53:44 +0100

    debug copy/paste error in cli parse arg utility

--- src/monitis_dump_monitor_list.py
+++ src/monitis_dump_monitor_list.py
@@ -1,14 +1,14 @@
 from src import settings
 from src.monitisapi import api_connector
 from src.monitisapi import data_model
-import utils
+from src import utils
 
 
 def main():
     parser = utils.get_output_arg_parser(
         description='Dump the list of monitor in Monitis into a CSV file.',
         default_credentials=settings.monitisapi["credentials"]["client_secret_path"],
-        default_input_file=settings.monitisapi["monitor"]["dump_file"])
+        default_output_file=settings.monitisapi["monitor"]["dump_file"])
     args = parser.parse_args()
     service = api_connector.Service(args.credentials)
     res = service.list_monitors()
--- src/utils.py
+++ src/utils.py
@@ -1,13 +1,13 @@
 import argparse
 
 
-def get_output_arg_parser(description="", default_credentials="", default_input_file=""):
+def get_output_arg_parser(description="", default_credentials="", default_output_file=""):
     parser = argparse.ArgumentParser(description=description)
     parser.add_argument('--credentials', dest="credentials",
                         default=default_credentials,
                         help='path to the credentials file to use to authenticate over the API.')
     parser.add_argument('--output', dest="dump_file",
-                        default=default_input_file,
+                        default=default_output_file,
                         help='path of the file where the data must be written')
     return parser
 
@@ -17,7 +17,7 @@ def get_input_arg_parser(description="", default_credentials="", default_input_f
     parser.add_argument('--credentials', dest="credentials",
                         default=default_credentials,
                         help='path to the credentials file to use to authenticate over the API.')
-    parser.add_argument('--output', dest="dump_file",
+    parser.add_argument('--input', dest="input_file",
                         default=default_input_file,
                         help='path of the file to use as the data input')
     return parser
