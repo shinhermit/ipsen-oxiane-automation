4b8fd1f7376efd423a6cba743efc60aa7bc572b3
Sebastien Poirot <sebastien.poirot@gmx.fr>
2017-12-07 11:26:12 +0100

    refactoring of the google_search_console adding script

--- src/google_search_console_add_from_analytics_property_list.py
+++ src/google_search_console_add_from_analytics_property_list.py
@@ -7,6 +7,7 @@ from src import settings
 from src.googleapi.api_connector import get_service
 from src.googleapi.analyticss.data_model import AccountSummaryList
 from src import utils
+import csv
 
 
 def main():
@@ -14,29 +15,20 @@ def main():
                                                     "list of google analytics properties from a CSV file.")
     args = parser.parse_args()
 
-    analytics_settings = settings.googleapi["analytics"]
-    api_analytics = get_service(api_name=analytics_settings["api_name"],
-                                api_version=analytics_settings['api_version'],
-                                client_secrets_path=args.credentials,
-                                scope=analytics_settings['scopes'])
-
     search_console_settings = settings.googleapi["search_console"]
     api_search_console = get_service(api_name=search_console_settings["api_name"],
                                      api_version=search_console_settings['api_version'],
                                      client_secrets_path=args.credentials,
                                      scope=search_console_settings['scopes'])
 
-    account_summaries = api_analytics.management().accountSummaries().list().execute(num_retries=10)
-    acc_list = AccountSummaryList(account_summaries)
-
     batch = BatchHttpRequest()
-    print("accounts: ", len(acc_list.items))
-    nb_web_properties = 0
-    for account in acc_list.items:
-        nb_web_properties += len(account.web_properties)
-        for web_property in account.web_properties:
-            batch.add(api_search_console.sites().add(siteUrl=web_property.website_url))
-    print("web properties", nb_web_properties)
+    with open(args.input_file, 'r') as csv_file:
+        reader = csv.DictReader(csv_file)
+        for row in reader:
+            website_url = row["Properties"]
+            account = row["Account"]
+            print("Currently in the %s Account, adding %s to Google Search Console" % (account, website_url))
+            batch.add(api_search_console.sites().add(siteUrl=website_url))
     batch.execute()
 
 
