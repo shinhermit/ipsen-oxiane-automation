07fe2118344d57f4dd7ae09fd017e56a7874af06
Josuah ARON <josuah.aron@gmail.com>
2017-12-07 10:12:42 +0100

    add command line arguments to the 'google analytics dump properties' script

--- src/google_analytics_dump_property_list.py
+++ src/google_analytics_dump_property_list.py
@@ -3,27 +3,30 @@ This script creates a list of all the properties (and the corresponding account)
 from a given Google Analytics Account
 """
 import csv
-import logging
 import re
 from src import settings
 from src.googleapi.api_connector import get_service
 from src.googleapi.analyticss.data_model import AccountSummaryList
+from src import utils
 
 
 def main():
-    client_secrets_path = settings.googleapi["credentials"]['client_secret_path']
+    parser = utils.get_output_arg_parser(description="Dump the list of all Google Analytics properties.",
+                                         default_credentials=settings.googleapi["credentials"]['client_secret_path'],
+                                         default_output_file=settings.googleapi['analytics']['dump_file'])
+    args = parser.parse_args()
 
     analytics_settings = settings.googleapi["analytics"]
     api_analytics = get_service(api_name=analytics_settings["api_name"],
                                 api_version=analytics_settings['api_version'],
-                                client_secrets_path=client_secrets_path,
+                                client_secrets_path=args.credentials,
                                 scope=analytics_settings['scopes'])
 
     account_summaries = api_analytics.management().accountSummaries().list().execute(num_retries=10)
     acc_list = AccountSummaryList(account_summaries)
 
-    with open(settings.googleapi['analytics']['dump_file'], 'w+', newline='') as csvfile:
-        wr = csv.writer(csvfile)
+    with open(args.dump_file, 'w+', newline='') as csv_file:
+        wr = csv.writer(csv_file)
         wr.writerow(("Account Id", "Account", "Properties Id", "Properties", "Without URL"))
         for account in acc_list.items:
             for web_property in account.web_properties:
